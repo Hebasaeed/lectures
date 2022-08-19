d967ce3f9e67b39451888ca9c0ed89dfdfc31962
Heba Saeed <88768344+Hebasaeed@users.noreply.github.com>
2022-08-19 18:02:32 +0200

    modified

--- task2.php
+++ task2.php
@@ -44,15 +44,109 @@ declare(strict_types=1); // to give red line under  not match  data type word
 // echo next_letter("u");
 
 
-###################################
- $x=10;
-function printy(){
-  global $x;                 #take it from global scope
-   $y=20;
-   return $x;
-}
 
-echo printy();
 
+#############       types of array       ##################
+
+
+//  1 =====> indexed array  
+##############################
+
+// $x=["ahmed", 19 , 40]; 
+//   // === or ===>>  //$x=  array("ahmed", 19 , 40);
+// //print_r($x);
+// foreach($x as $key => $values){
+//   echo  $key ."=>" .$values . "<br>";
+// }
+
+// foreach($x as$values){
+//   echo  $values . "<br>";
+// }
+
+###########################################
+
+//  2  ======>  associative array
+####################################
+
+// $x=["name "=> "ahmed", "gpa "  => 19 , "age"  =>  40]; 
+// foreach($x as $key => $values){
+//   echo  $key ."=>" .$values . "<br>";
+// }
+
+
+###############################################
+
+//  3  ======>  multi dymentional array ==>array of arrays
+###########################################################3
+//  $x=[
+//   ["name" => "ahmed" , "gpa"  => 19 , "age"  =>  40]
+//   ,["name" => "ahmed" , "gpa"  => 19 , "age"  =>  40]
+//   ,["name" => "ahmed" , "gpa"  => 19 , "age"  =>  40]
+//   ,["name" => "ahmed" , "gpa"  => 19 , "age"  =>  40]
+//   , 58
+//   ,"heba"
+//  ]; 
+ 
+//  foreach($x as $key => $values){
+  
+//   foreach($values as $key => $raws){
+//   echo  $key ."=>" .$raws. "<br>";
+  
+
+// }
+// //echo  $key ."=>" .$values. "<br>";
+// echo "<br>";
+//  }
+// //print_r($x);
+// //echo $x[3]["name"];
+###############################################
+
+// $y=["a","b","e","c"];
+// rsort($y);
+// sort($y);
+
+################################################
+// $y=["a"=>2,"b"=>6,"e"=>3,"c"=>5];
+// asort($y);
+// // asort   & arsort  ==> value   arrange
+// // ksort  & krsort ==> key   arrange
+// print_r($y);
+
+//print_r($GLOBALS);
+
+####################################################
+
+####################    GLOBALS  ###############
+//  $x=10;
+// function printy(){
+//   global $x;                 #take it from global scope
+//    
+//    return $x;
+// }
+
+// echo printy();
+
+############## or ############
+
+// $x=10;
+// function printy(){  
+   
+//    return $GLOBALS['x'];         #take it from global scope
+// }
+
+// echo printy();
+
+
+######################## types of super globals ########################
+##  supper globals deal as arrays
+
+
+#echo $_SERVER['PHP_SELF']; // LOCATION OF THIS FILE
+#echo $_SERVER['SERVER_NAME']; // name of server => localhost
+#echo $_SERVER['HTTP_HOST'];  // name of HOST => localhost
+#echo $_SERVER['SCRIPT_NAME']; // LIKE LOCATION OF THIS FILE
+#echo $_SERVER['SERVER_SOFTWARE']; // details of software
+#echo $_SERVER['REMOTE_ADDR'];  // details  ip of user or me that enter this server
+#echo $_SERVER['SERVER_ADDR']; // ip of server that you are hosting
 
 ?>
