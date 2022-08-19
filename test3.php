44e8d27389d40d5482a6de50031f2d654db2cd09
Heba Saeed <88768344+Hebasaeed@users.noreply.github.com>
2022-08-19 18:33:08 +0200

    add new file

---
+++ test3.php
@@ -0,0 +1,24 @@
+<?php
+
+//GET & POST
+/*
+sensetivity (post) data is sensetive and send sensetive data
+visibilty  (get)data send in url and visible
+limition   (get) have limit data ===> volume until 2048 character
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
